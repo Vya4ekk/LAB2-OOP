40de3e87c0f5f6ca005c44f16a451275b0528e41
Vya4ekk <89924475+Vya4ekk@users.noreply.github.com>
2023-06-08 08:32:12 +0300

    LAB-2-OOP.cpp

--- LAB-2-OOP.cpp
+++ LAB-2-OOP.cpp
@@ -1,4 +1,4 @@
-﻿#include "my_queue.h"
+#include <D:\Projects\Static_library\My_queue_library\My_queue.h>
 
 using namespace std;
 
@@ -8,91 +8,124 @@ int main()
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
-	que q;
+	que* qarr = new que[10];
 	people p;
 	string filename = "", name = "no_name";
-	int priority = 1, l = 0, r = 0;
+	int priority = 1, l = 0, r = 0, i = 0, l1 = 0, r1 = 0;
 
-	int nfun = 1;
-	while (nfun != 0)
+	int nfun = 0, nq = 0;
+	while (nfun != -1)
 	{
-		cout << "0 = Записать данные в файл и выйти\n"
+		cout << "0 = Записать данные в файл\n"
 			<< "1 = Показать очередь\n"
 			<< "2 = Добавить элемент в очередь\n"
 			<< "3 = Удалить передний элемент из очереди\n"
 			<< "4 = Ввести данные из файла в очередь\n"
-			<< "5 = Реализация \"Псевдозминной\"\n"
-			<< "6 = показать передний элемент очереди\n"
-			<< "7 = Рассортировать по приоритету\n\n"
+			<< "5 = Реализация \"Псевдозминной\" с очередью\n"
+			<< "6 = Реализация \"Псевдозминной\" с человеком\n"
+			<< "7 = Показать передний элемент очереди\n"
+			<< "8 = Рассортировать по приоритету\n"
+			<< "9 = Перейти в управление к новой очереди\n"
+			<< "10 = Слияние очередей\n"
+			<< "-1 = Завершить и выйти\n\n"
 			<< "Введите номер функции ";
 		cin >> nfun;
+		system("cls");
 		switch (nfun)
 		{
 		case 0:
-			system("cls");
-			q.file(filename);
+			cout << "Введите путь к файлу куда записать очередь\n";
+			cin.ignore();
+			SetConsoleCP(1251);
+			SetConsoleOutputCP(1251);
+			getline(cin, filename);
+			qarr[nq].file(filename);
+			cout << "Успешно записано\n";
 			system("pause");
 			break;
 		case 1:
-			system("cls");
-			q.show();
+			qarr[nq].show();
 			system("pause");
 			break;
 		case 2:
-			system("cls");
 			cout << "Введите имя и приоритет, которое добавим в очередь\nИмя: ";
+			SetConsoleCP(1251);
+			SetConsoleOutputCP(1251);
 			cin >> name;
 			cin.clear();
 			cin.ignore();
 			cout << "Приритет: "; cin >> priority;
 			p.name = name;
 			p.priority = priority;
-			q.enqueue(p);
+			qarr[nq].push(p);
 			system("pause");
 			break;
 		case 3:
 			cout << endl;
-			q.dequeue();
+			qarr[nq].pop();
 			system("pause");
 			break;
 		case 4:
-			system("cls");
 			cout << "Введите имя файла ";
 			cin.clear();
 			cin.ignore();
 			getline(cin, filename);
-			q.getfromfile(filename);
+			qarr[nq].getfromfile(filename);
 			break;
 		case 5:
-			system("cls");
+			cout << "Введите границы очереди для изменения\n"
+				<< "Левая "; cin >> l; cin.clear();
+			cout << "Правая "; cin >> r; cin.clear();
+			cout << "Введите номер очереди которую вставить в данную ";
+			cin >> i; cin.clear();
+			cout << "Введите границы из добавляемой очереди\n"
+				<< "Левая "; cin >> l1; cin.clear();
+			cout << "Правая "; cin >> r1; cin.clear();
+			qarr[nq](l, r) = qarr[i](l1, r1);
+			break;
+		case 6:
 			cout << "Введите границы очереди для изменения\n"
 				<< "Левая "; cin >> l;
 			cin.clear();
 			cout << "Правая "; cin >> r;
 			cin.clear();
 			cout << "Введите данные о человеке\n"
-				<< "Имя ";  cin >> name;
+				<< "Имя ";
+			SetConsoleCP(1251);
+			SetConsoleOutputCP(1251);
+			cin >> name;
 			p.name = name;
 			cin.clear();
 			cout << "Приоритет "; cin >> priority;
 			p.priority = priority;
-			q(l, r, p);
-			break;
-		case 6:
-			system("cls");
-			cout << "Имя: " << q.peek().name << "\t\tПриоритет: " << q.peek().priority << endl;
-			system("pause");
+			qarr[nq](l, r) = p;
 			break;
 		case 7:
-			q.setpriority();
-			system("cls");
+			cout << "Имя: " << qarr[nq].peek().name << "\t\tПриоритет: " << qarr[nq].peek().priority << endl;
+			system("pause");
+			break;
+		case 8:
+			qarr[nq].setpriority();
 			cout << "Отсортированно\n";
 			break;
+		case 9:
+			cout << "Введите номер нужной очереди ";
+			cin >> nq;
+			cout << "Управление передано\n";
+			system("pause");
+			break;
+		case 10:
+			cout << "Введите номер очереди для сливания ";
+			cin >> i;
+			qarr[nq] + qarr[i];
+			cout << "Слияние произошло\n";
+			system("pause");
+			break;
 		default:
-			cout << "Неправильно введен номер" << endl;
 			break;
 		}
 		cin.clear();
+		filename.clear();
 		system("cls");
 	}
 
