9928ec1d75ea7cbec5c456406cb57d38551d62d6
Vya4ekk <89924475+Vya4ekk@users.noreply.github.com>
2023-06-08 08:32:49 +0300

    my_queue.h

--- my_queue.h
+++ my_queue.h
@@ -7,30 +7,33 @@
 #include <iterator>
 #include <sstream>
 
+using namespace std;
+
 struct people
 {
     int priority = 0;
-    std::string name = "no_name";   
+    std::string name = "no_name";
 };
 
 class que
 {
-    people *arr;
+    people* arr;
     int capacity;
     int rear;
     int count;
 
+    int left = -1;
+    int right = -1;
 public:
     que(int size = 200);
     que(const que& q);
     ~que();
 
-    void dequeue();
-    void enqueue(people x);
+    void pop();
+    void push(people x);
     void incapacity(int newcapacity = 10);
     void show();
     void file(std::string filename);
-    void getfromfile(std::string filename);
     void setpriority();
 
     people peek();
@@ -39,9 +42,13 @@ public:
 
     bool isEmpty();
     bool isFull();
-    
-    people& operator = (const people & p);
-    que& operator = (const que& q);
-    que& operator ()(int l, int r, const people& p);
-};
 
+    que& getfromfile(std::string filename);
+    que& operator+(const que& q);
+    que& operator=(const people& p);
+    que& operator = (const que& q);
+    que& operator ()(int l, int r);
+
+    friend ostream& operator <<(std::ostream& os, const que& q);
+    friend istream& operator >>(std::istream& is, que& q);
+};
